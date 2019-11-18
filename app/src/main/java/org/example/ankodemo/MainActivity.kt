46be19df308fd62de511b59e4cc7a8cb306f5cc3
xdeepakv <deepak.m.shrma@gmail.com>
2019-11-19 01:29:38 +0800

    Upgrade example app to AndroidX

--- app/src/main/java/org/example/ankodemo/MainActivity.kt
+++ app/src/main/java/org/example/ankodemo/MainActivity.kt
@@ -1,12 +1,12 @@
 package org.example.ankodemo
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
 import android.text.InputType.TYPE_CLASS_TEXT
 import android.text.InputType.TYPE_TEXT_VARIATION_PASSWORD
 import android.view.Gravity
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.coroutines.onClick
 
